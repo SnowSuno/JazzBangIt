dc560607c0dc46e2cd9c0f6e018eb7fb279836fd
SnowSuno <percy3368@gmail.com>
2021-09-25 00:38:18 +0900

    feat : apply roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import rootReducer from "./reducers";
