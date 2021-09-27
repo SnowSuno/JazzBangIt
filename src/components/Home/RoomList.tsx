58e579e8f7dc9259820c7c018d67186797b09f62
SnowSuno <percy3368@gmail.com>
2021-09-27 22:57:35 +0900

    feat : add loading, error indicator

--- src/components/Home/RoomList.tsx
+++ src/components/Home/RoomList.tsx
@@ -4,14 +4,36 @@ import "./RoomList.scss";
 import {useSelector} from "react-redux";
 import {RootState} from "../../reducers";
 
+import { Skeleton } from '@mui/material';
+import SignalWifiOffIcon from '@mui/icons-material/SignalWifiOff';
+import MusicOffIcon from '@mui/icons-material/MusicOff';
+
 import Room from "./Room";
 
 function RoomList() {
-    const rooms = useSelector((state: RootState) => state.api.data);
-
+    const {data, loading, error} = useSelector((state: RootState) => state.api);
     return (
         <div className="RoomList">
-            {rooms.map((room, index) => <Room room={room} key={index} />)}
+            {loading
+                ? Array(5).fill(<Skeleton
+                    sx={{bgcolor: "#FBFBFC", borderRadius: "12px"}}
+                    height={87}
+                    width={"100%"}
+                    variant="rectangular"
+                    animation="wave"
+                />)
+                : error
+                    ? <div className="no-room">
+                        <SignalWifiOffIcon sx={{fontSize: 150, color: "#AFB1B4"}}/>
+                        <p>인터넷 연결 안됨</p>
+                    </div>
+                    : (data.length > 0)
+                        ? data.map((room, index) => <Room room={room} key={index} />)
+                        : <div className="no-room">
+                            <MusicOffIcon sx={{fontSize: 150, color: "#AFB1B4"}}/>
+                            <p>재즈방 없어요...</p>
+                        </div>
+            }
         </div>
     );
 }
